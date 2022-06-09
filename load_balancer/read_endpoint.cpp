b75447098202f746542ee70331efee2f40dd9ad6
Sierra Wang <sierrawang@ucsb.edu>
2022-06-09 04:11:36 +0000

    fix read_endpoint and tweet_db

--- load_balancer/read_endpoint.cpp
+++ load_balancer/read_endpoint.cpp
@@ -1,74 +1,105 @@
+#include "../gen-cpp/read_endpoint.h"
+#include "ports.h"
+#include <iostream>
+#include <map>
 #include <thrift/protocol/TBinaryProtocol.h>
 #include <thrift/server/TThreadedServer.h>
 #include <thrift/transport/TServerSocket.h>
 #include <thrift/transport/TSocket.h>
 #include <thrift/transport/TTransportUtils.h>
-#include <iostream>
-#include "../gen-cpp/read_endpoint.h"
 #include <vector>
-#include "ports.h"
-#include <map>
+
+#include <chrono>
+#include <ctime>
+#include <sys/time.h>
 
 using namespace load_balancer;
+using std::chrono::duration_cast;
+using std::chrono::microseconds;
+using std::chrono::system_clock;
 
 class read_endpointHandler : public read_endpointIf {
 public:
   read_endpointHandler() = default;
 
   bool update_feed(const std::string &username, const Tweet &tweet) override {
-    std::cout << "read_endpoint\t update_feed" << std::endl;
+    std::cout << "read_endpoint\t update_feed a " << username << " "
+              << tweet.content << " "
+              << duration_cast<microseconds>(
+                     system_clock::now().time_since_epoch())
+                     .count()
+              << std::endl;
     auto u = std::string(username);
     auto it = m_feeds.find(u);
     if (it == m_feeds.end()) {
-        m_feeds.emplace(u, std::vector<Tweet>());
-        it = m_feeds.find(u);
+      m_feeds.emplace(u, std::vector<Tweet>());
+      it = m_feeds.find(u);
     }
     it->second.push_back(tweet);
+    std::cout << "read_endpoint\t update_feed b " << username << " "
+              << tweet.content << " "
+              << duration_cast<microseconds>(
+                     system_clock::now().time_since_epoch())
+                     .count()
+              << std::endl;
     return true;
   }
 
   void get_feed(std::vector<Tweet> &_return, const std::string &username) {
-        auto u = std::string(username);
-        auto it = m_feeds.find(u);
-        if (it == m_feeds.end()) {
-            _return = std::vector<Tweet>();
-        } else {
-            _return = it->second;
-        }
+    std::cout << "read_endpoint\t get_feed a " << username << " "
+              << duration_cast<microseconds>(
+                     system_clock::now().time_since_epoch())
+                     .count()
+              << std::endl;
+    auto u = std::string(username);
+    auto it = m_feeds.find(u);
+    if (it == m_feeds.end()) {
+      _return = std::vector<Tweet>();
+    } else {
+      _return = it->second;
+    }
+    std::cout << "read_endpoint\t get_feed b " << username << " "
+              << duration_cast<microseconds>(
+                     system_clock::now().time_since_epoch())
+                     .count()
+              << std::endl;
   }
 
   std::map<std::string, std::vector<Tweet>> m_feeds;
 };
 
 class read_endpointFactory : virtual public read_endpointIfFactory {
-    public:
-    read_endpointFactory() {
-        handler = new read_endpointHandler;
-    }
+public:
+  read_endpointFactory() { handler = new read_endpointHandler; }
 
-    ~read_endpointFactory() override = default;
-    
-    read_endpointIf* getHandler(const ::apache::thrift::TConnectionInfo& connInfo) override
-    {
-        std::shared_ptr<::apache::thrift::transport::TSocket> sock = std::dynamic_pointer_cast<::apache::thrift::transport::TSocket>(connInfo.transport);
-        return handler;
-    }
+  ~read_endpointFactory() override = default;
 
-    void releaseHandler(read_endpointIf* handler) override {
-        // delete handler;
-    }
+  read_endpointIf *
+  getHandler(const ::apache::thrift::TConnectionInfo &connInfo) override {
+    std::shared_ptr<::apache::thrift::transport::TSocket> sock =
+        std::dynamic_pointer_cast<::apache::thrift::transport::TSocket>(
+            connInfo.transport);
+    return handler;
+  }
 
-    read_endpointIf* handler;
+  void releaseHandler(read_endpointIf *handler) override {
+    // delete handler;
+  }
+
+  read_endpointIf *handler;
 };
 
 int main() {
-    apache::thrift::server::TThreadedServer server(
-        std::make_shared<read_endpointProcessorFactory>(std::make_shared<read_endpointFactory>()),
-        std::make_shared<apache::thrift::transport::TServerSocket>(READ_ENDPT_PORT),
-        std::make_shared<apache::thrift::transport::TBufferedTransportFactory>(),
-        std::make_shared<apache::thrift::protocol::TBinaryProtocolFactory>());
+  apache::thrift::server::TThreadedServer server(
+      std::make_shared<read_endpointProcessorFactory>(
+          std::make_shared<read_endpointFactory>()),
+      std::make_shared<apache::thrift::transport::TServerSocket>(
+          READ_ENDPT_PORT),
+      std::make_shared<apache::thrift::transport::TBufferedTransportFactory>(),
+      std::make_shared<apache::thrift::protocol::TBinaryProtocolFactory>());
 
-    std::cout << "Starting read_endpoint on port " << READ_ENDPT_PORT << std::endl;
-    server.serve();
-    return 0;
+  std::cout << "Starting read_endpoint on port " << READ_ENDPT_PORT
+            << std::endl;
+  server.serve();
+  return 0;
 }
--- load_balancer/tweet_db.cpp
+++ load_balancer/tweet_db.cpp
@@ -6,15 +6,15 @@
 #include <thrift/transport/TSocket.h>
 #include <thrift/transport/TTransportUtils.h>
 
-#include "ports.h"
 #include "../gen-cpp/tweet_db.h"
+#include "ports.h"
 
+#include <chrono>
+#include <ctime>
 #include <map>
 #include <stdio.h>
-#include <vector>
-#include <chrono>
 #include <sys/time.h>
-#include <ctime>
+#include <vector>
 
 using namespace load_balancer;
 using std::chrono::duration_cast;
@@ -22,87 +22,122 @@ using std::chrono::microseconds;
 using std::chrono::system_clock;
 
 class TweetDbHandler : public tweet_dbIf {
-    public:
-    TweetDbHandler() = default;
-    
-    bool delete_user(const std::string& username) override {
-        std::string u = std::string(username);
-        auto user = tweets.find(u);
-        if (user != tweets.end()) {
-            tweets.erase(user);
-        }
-        return true;
+public:
+  TweetDbHandler() = default;
+
+  bool delete_user(const std::string &username) override {
+    std::cout << "tweet_db:\t delete_user() a " << username << " "
+              << duration_cast<microseconds>(
+                     system_clock::now().time_since_epoch())
+                     .count()
+              << std::endl;
+    std::string u = std::string(username);
+    auto user = tweets.find(u);
+    if (user != tweets.end()) {
+      tweets.erase(user);
+    }
+    std::cout << "tweet_db:\t delete_user() b " << username << " "
+              << duration_cast<microseconds>(
+                     system_clock::now().time_since_epoch())
+                     .count()
+              << std::endl;
+    return true;
+  }
+
+  void post_tweet(Tweet &_return, const std::string &username,
+                  const std::string &tweet) override {
+    std::cout << "tweet_db:\t post_tweet() a " << username << " " << tweet
+              << " "
+              << duration_cast<microseconds>(
+                     system_clock::now().time_since_epoch())
+                     .count()
+              << std::endl;
+    std::string u = std::string(username);
+
+    Tweet t;
+    t.username = u;
+    t.content = tweet;
+    t.timestamp =
+        duration_cast<microseconds>(system_clock::now().time_since_epoch())
+            .count();
+    t.id = cur_id;
+    cur_id++;
+
+    auto search = tweets.find(u);
+    if (search != tweets.end()) {
+      auto &user_tweets = search->second;
+      user_tweets.push_back(t);
+    } else {
+      tweets.emplace(u, std::vector<Tweet>{t});
     }
 
-    // TO DO - figure out if I need to syncronize this
-    void post_tweet(Tweet& _return, const std::string& username, const std::string& tweet) override {
-        std::string u = std::string(username);
-        std::cout << "tweet_db:\t post_tweet(), " << username << " " << tweet << std::endl;
+    _return = t;
 
-        Tweet t;
-        t.username = u;
-        t.content = tweet;
-        t.timestamp = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();;
-        t.id = cur_id;
-        cur_id++;
+    std::cout << "tweet_db:\t post_tweet() b " << username << " " << tweet
+              << " "
+              << duration_cast<microseconds>(
+                     system_clock::now().time_since_epoch())
+                     .count()
+              << std::endl;
+  }
 
-        auto search = tweets.find(u);
-        if (search != tweets.end()) {
-            auto& user_tweets = search->second;
-            user_tweets.push_back(t);
-        } else {
-            tweets.emplace(u, std::vector<Tweet>{t});
-        }
+  void get_user_tweets(std::vector<Tweet> &_return,
+                       const std::string &username) override {
+    std::cout << "tweet_db:\t get_user_tweets() a " << username << " "
+              << duration_cast<microseconds>(
+                     system_clock::now().time_since_epoch())
+                     .count()
+              << std::endl;
 
-        _return  = t;
+    std::string u = std::string(username);
+
+    auto search = tweets.find(u);
+    if (search != tweets.end()) {
+      _return = search->second;
+    } else {
+      _return = std::vector<Tweet>();
     }
+    std::cout << "tweet_db:\t get_user_tweets() b " << username << " "
+              << duration_cast<microseconds>(
+                     system_clock::now().time_since_epoch())
+                     .count()
+              << std::endl;
+  }
 
-    void get_user_tweets(std::vector<Tweet> & _return, const std::string& username) override {
-        std::string u = std::string(username);
-        std::cout << "tweet_db:\t get_user_tweets()" << u << std::endl;
-
-        auto search = tweets.find(u);
-        if (search != tweets.end()) {
-            _return = search->second;
-        } else {
-            _return = std::vector<Tweet>();
-        }
-    }
-
-    private:
-    uint64_t cur_id;
-    std::map<std::string, std::vector<Tweet>> tweets;
+private:
+  uint64_t cur_id;
+  std::map<std::string, std::vector<Tweet>> tweets;
 };
 
 class TweetDbCloneFactory : virtual public tweet_dbIfFactory {
-    public:
-    TweetDbCloneFactory() {
-        handler = new TweetDbHandler;
-    }
-    ~TweetDbCloneFactory() override = default;
+public:
+  TweetDbCloneFactory() { handler = new TweetDbHandler; }
+  ~TweetDbCloneFactory() override = default;
 
-    tweet_dbIf* getHandler(const ::apache::thrift::TConnectionInfo& connInfo) override {
-        std::shared_ptr<apache::thrift::transport::TSocket> sock =
-            std::dynamic_pointer_cast<apache::thrift::transport::TSocket>(connInfo.transport);
-        return handler;
-    
-    }
+  tweet_dbIf *
+  getHandler(const ::apache::thrift::TConnectionInfo &connInfo) override {
+    std::shared_ptr<apache::thrift::transport::TSocket> sock =
+        std::dynamic_pointer_cast<apache::thrift::transport::TSocket>(
+            connInfo.transport);
+    return handler;
+  }
 
-    void releaseHandler(tweet_dbIf* handler) override {
-        // delete handler;
-    }
+  void releaseHandler(tweet_dbIf *handler) override {
+    // delete handler;
+  }
 
-    tweet_dbIf* handler;
+  tweet_dbIf *handler;
 };
 
 int main() {
-    apache::thrift::server::TThreadedServer server(
-        std::make_shared<tweet_dbProcessorFactory>(std::make_shared<TweetDbCloneFactory>()),
-        std::make_shared<apache::thrift::transport::TServerSocket>(TWEETDB_PORT),
-        std::make_shared<apache::thrift::transport::TBufferedTransportFactory>(),
-        std::make_shared<apache::thrift::protocol::TBinaryProtocolFactory>());
+  apache::thrift::server::TThreadedServer server(
+      std::make_shared<tweet_dbProcessorFactory>(
+          std::make_shared<TweetDbCloneFactory>()),
+      std::make_shared<apache::thrift::transport::TServerSocket>(TWEETDB_PORT),
+      std::make_shared<apache::thrift::transport::TBufferedTransportFactory>(),
+      std::make_shared<apache::thrift::protocol::TBinaryProtocolFactory>());
 
-    std::cout << "Starting tweet_db on port " << TWEETDB_PORT << std::endl;
-    server.serve();
-    return 0;
+  std::cout << "Starting tweet_db on port " << TWEETDB_PORT << std::endl;
+  server.serve();
+  return 0;
 }

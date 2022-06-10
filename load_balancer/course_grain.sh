02467b11f933a26cddca6e46a80778693095f024
Sierra Wang <sierrawang@ucsb.edu>
2022-06-10 20:25:20 +0000

    Make sure the containers run one at a time

--- load_balancer/course_grain.sh
+++ load_balancer/course_grain.sh
@@ -6,11 +6,11 @@ docker rm -f read_edpt
 
 docker run -p 3003:3000 --net mynet --name read_edpt -v /home/ubuntu/AmbienceBenchmarksThrift:/root/app thrift_img /bin/bash load_balancer/run_read_edpt.sh &
 
-sleep 1
+sleep 5
 
 docker run -p 3000:3000 --net mynet --name db_con -v /home/ubuntu/AmbienceBenchmarksThrift:/root/app thrift_img /bin/bash load_balancer/run_db.sh &
 
-sleep 1
+sleep 5
 
 docker run -p 3001:3000 --net mynet --name worker_con -v /home/ubuntu/AmbienceBenchmarksThrift:/root/app thrift_img /bin/bash load_balancer/run_worker.sh &
 

2d21563dbda18356d44fcf9203da634a9da95877
Sierra Wang <sierrawang@ucsb.edu>
2022-06-10 20:13:22 +0000

    Added test scripts for course grain

---
+++ load_balancer/course_grain.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+docker rm -f worker_con
+docker rm -f db_con
+docker rm -f read_edpt
+
+docker run -p 3003:3000 --net mynet --name read_edpt -v /home/ubuntu/AmbienceBenchmarksThrift:/root/app thrift_img /bin/bash load_balancer/run_read_edpt.sh &
+
+sleep 1
+
+docker run -p 3000:3000 --net mynet --name db_con -v /home/ubuntu/AmbienceBenchmarksThrift:/root/app thrift_img /bin/bash load_balancer/run_db.sh &
+
+sleep 1
+
+docker run -p 3001:3000 --net mynet --name worker_con -v /home/ubuntu/AmbienceBenchmarksThrift:/root/app thrift_img /bin/bash load_balancer/run_worker.sh &
+
+sleep 5
+
+python3 load_balancer_client.py > course_grain_results/linux_course_grain_parsed.txt
+
+echo DONE!
--- load_balancer/load_balancer_client.py
+++ load_balancer/load_balancer_client.py
@@ -12,34 +12,25 @@ from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
-# Make socket
-transport = TSocket.TSocket('worker_con', 9089)
-
-# Buffering is critical. Raw sockets are very slow
-transport = TTransport.TBufferedTransport(transport)
-
-# Wrap in a protocol
-protocol = TBinaryProtocol.TBinaryProtocol(transport)
-
-# Create a client to use the protocol encoder
-m_load_balancer = worker.Client(protocol)
-
-# Connect!
-transport.open()
-
-
-
-# Close!
-transport.close()
 
 
 def sequence1(num_users, num_active_users, num_follow, num_posts):
     print("sequence1", num_users, num_follow, num_posts)
 
-    # Connect to load_balancer
-    ip = "127.0.0.1"
-    port = 1234
-    m_load_balancer = lidlrt.udp_client(tos.ae.services.worker.worker, (ip, port))
+    # Make socket
+    transport = TSocket.TSocket('172.18.0.4', 9089)
+
+    # Buffering is critical. Raw sockets are very slow
+    transport = TTransport.TBufferedTransport(transport)
+
+    # Wrap in a protocol
+    protocol = TBinaryProtocol.TBinaryProtocol(transport)
+
+    # Create a client to use the protocol encoder
+    m_load_balancer = worker.Client(protocol)
+
+    # Connect!
+    transport.open()
 
     # Generate random usernames
     letters = string.ascii_letters
@@ -55,27 +46,35 @@ def sequence1(num_users, num_active_users, num_follow, num_posts):
         posts.append(s)
 
     # Test create user
+    print("Testing create_user")
     for u in usernames:
-        m_load_balancer.create_user(username=u)
+        m_load_balancer.create_user(u)
     
     # Test follow
+    print("Testing follow")
     for i in range(num_active_users):
         for j in range(num_follow):
-            m_load_balancer.follow(follower=usernames[i], followee=usernames[(i + j + 1) % num_users])
+            m_load_balancer.follow(usernames[i], usernames[(i + j + 1) % num_users])
 
     # Test post tweet
+    print("Testing post_tweet")
     for i in range(num_active_users):
         for post in posts:
-            m_load_balancer.post_tweet(username=usernames[i], tweet=post) 
+            m_load_balancer.post_tweet(usernames[i], post) 
 
     # Test unfollow
+    print("Testing unfollow")
     for i in range(num_active_users):
         for j in range(num_follow):
-            m_load_balancer.unfollow(follower=usernames[i], followee=usernames[(i + j + 1) % num_users])
+            m_load_balancer.unfollow(usernames[i], usernames[(i + j + 1) % num_users])
 
     # Test delete_user
+    print("Testing delete_user")
     for u in usernames:
-        m_load_balancer.delete_user(username=u)
+        m_load_balancer.delete_user(u)
+
+    # Close!
+    transport.close()
 
 def start():
     num_users = 10000
@@ -84,7 +83,7 @@ def start():
     num_posts = 20
     sequence1(num_users, num_active_users, num_follow, num_posts)
 
-# start()
+start()
 
 # m_load_balancer.create_user(username="1")
 # m_load_balancer.create_user(username="2")

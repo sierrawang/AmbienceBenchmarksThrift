8170e0bb4fe42c4d15dd57d1f0e9b7d5ff2de804
Sierra Wang <sierrawang@ucsb.edu>
2022-06-16 20:50:17 +0000

    fix names and scripts

--- benchmark/scripts/run_parse_within_container_latency.sh
+++ benchmark/scripts/run_parse_within_container_latency.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 parse_poll_output.py ../benchmark/results/latency_within_group_output.txt > ../benchmark/results/latency_within_group_parsed.txt
-python3 parse_poll_param_output.py ../benchmark/results/param_latency_within_group_output.txtt > ../benchmark/results/param_latency_within_group_parsed.txt
+python3 parse_poll_output.py ../results/latency_within_container_output.txt > ../results/latency_within_container_parsed.txt
+python3 parse_poll_param_output.py ../results/param_latency_container_group_output.txt > ../results/param_latency_within_container_parsed.txt
--- benchmark/scripts/run_poll_one_container.sh
+++ benchmark/scripts/run_poll_one_container.sh
@@ -5,5 +5,5 @@ cd poll
 # Start all services
 ./poll > ../benchmark/results/poll_output.txt &
 sleep 5
-./poll_bench_agent > ../benchmark/results/latency_within_group_output.txt &
-./poll_param_bench_agent > ../benchmark/results/param_latency_within_group_output.txt
+./poll_bench_agent > ../benchmark/results/latency_within_container_output.txt &
+./poll_param_bench_agent > ../benchmark/results/param_latency_within_container_output.txt

02f3b3a02fe26d5512003f60f9ed7c0f5ae9ea83
Sierra Wang <sierrawang@ucsb.edu>
2022-06-16 22:47:23 +0000

    fix container port error

--- benchmark/scripts/run_BEPS_basic2.sh
+++ benchmark/scripts/run_BEPS_basic2.sh
@@ -14,7 +14,7 @@ sleep 15
 docker run -p 3000:3000 --net mynet --name db_con -v /home/ubuntu/AmbienceBenchmarksThrift:/root/app thrift_img /bin/bash load_balancer/run_db.sh &
 sleep 5
 
-docker run -p 3001:3000 --net mynet --name worker_con -v /home/ubuntu/AmbienceBenchmarksThrift:/root/app thrift_img /bin/bash load_balancer/run_worker.sh &
+docker run -p 3001:3000 -p 9089:9089 --net mynet --name worker_con -v /home/ubuntu/AmbienceBenchmarksThrift:/root/app thrift_img /bin/bash load_balancer/run_worker.sh &
 sleep 5
 
 echo DONE!

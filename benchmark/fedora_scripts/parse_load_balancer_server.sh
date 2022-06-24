125be479931ed2c23fc5b43e7a8e53ec2e91c82f
Sierra Wang <sierrawang@ucsb.edu>
2022-06-23 17:55:23 -0700

    add args

--- benchmark/fedora_scripts/parse_load_balancer_server.sh
+++ benchmark/fedora_scripts/parse_load_balancer_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 parse_BEPS_output.py ../fedora_results/fedora_BEPS_server_output.txt > ../fedora_results/fedora_BEPS_server_parsed.txt
+python3 parse_BEPS_output.py $1 > ../fedora_results/$1_parsed.txt

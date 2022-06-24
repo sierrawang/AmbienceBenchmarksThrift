f88f4c26a0d191098a1b6d7660c3df37f910f9ad
Sierra Wang <sierrawang@ucsb.edu>
2022-06-24 23:44:26 +0000

    add thrift to image script

--- script.sh
+++ script.sh
@@ -2,6 +2,22 @@
 
 dnf -y upgrade
 
-dnf install -y wget thrift-devel libtool m4 automake byacc flex bison gcc-c++ git libevent-devel openssl-devel make pkg-config
+dnf install -y wget thrift-devel libtool m4 automake byacc flex bison gcc-c++ git libevent-devel openssl-devel make pkg-config boost-static boost-devel python3-devel
 
 dnf install -y vim
+
+git clone https://github.com/apache/thrift.git
+
+cd thrift
+
+./bootstrap.sh
+
+./configure
+
+make
+
+make install
+
+cd lib/py
+
+python3 setup.py install

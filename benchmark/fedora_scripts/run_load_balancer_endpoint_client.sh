d4830dc3315cddcd24ca9bc90d302bd277f70092
Sierra Wang <sierrawang@ucsb.edu>
2022-06-24 09:07:29 -0700

    run_load_balancer_endpoint_client

--- benchmark/fedora_scripts/run_load_balancer_endpoint_client.sh
+++ benchmark/fedora_scripts/run_load_balancer_endpoint_client.sh
@@ -3,7 +3,3 @@ cd ../../load_balancer/
 
 # Run the client
 python3 test_write_endpoint2.py
-
-cp ../results/BEPS_write_endpoint_linux_output.txt > ../fedora_results/fedora_BEPS_client_write_endpoint_output.txt
-
-python3 parse_BEPS_output.py ../fedora_results/fedora_BEPS_client_write_endpoint_output.txt > ../fedora_results/fedora_BEPS_client_write_endpoint_parsed.txt

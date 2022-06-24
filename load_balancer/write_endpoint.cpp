c4884ef07840526ead23000e92cac41515e8a98a
Sierra Wang <sierrawang@ucsb.edu>
2022-06-24 11:10:53 -0700

    Make write endpoint same as client

--- load_balancer/write_endpoint.cpp
+++ load_balancer/write_endpoint.cpp
@@ -24,103 +24,205 @@ class write_endpointHandler : public write_endpointIf {
 public:
   write_endpointHandler() = default;
 
-  bool sequence1(int num_users, int num_active_users, int num_follow,
-                 int num_post) {
-    std::shared_ptr<apache::thrift::transport::TTransport> socket(
-        new apache::thrift::transport::TSocket("10.0.1.10", LB_PORT));
-    std::shared_ptr<apache::thrift::transport::TTransport> transport(
-        new apache::thrift::transport::TBufferedTransport(socket));
-    std::shared_ptr<apache::thrift::protocol::TProtocol> protocol(
-        new apache::thrift::protocol::TBinaryProtocol(transport));
-    workerClient m_load_balancer(protocol);
+    // Random string generator inspired by
+    // https://stackoverflow.com/questions/440133/how-do-i-create-a-random-alpha-numeric-string-in-c
+    // and std::generate_n()
+    std::string random_string( size_t length )
+    {
+        const char charset[] =
+            "0123456789"
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            "abcdefghijklmnopqrstuvwxyz";
+        const auto max_index = (sizeof(charset) - 1);
 
-    transport->open();
-
-    uint64_t before = 0;
-    uint64_t after = 0;
-    uint64_t time = 0;
-
-    // std::cout << "test_create_user sequence1" << std::endl;
-    for (auto i = 0; i < num_users; i++) {
-      auto username = std::to_string(i);
-      before =
-          duration_cast<microseconds>(system_clock::now().time_since_epoch())
-              .count();
-      m_load_balancer.create_user(username);
-      after =
-          duration_cast<microseconds>(system_clock::now().time_since_epoch())
-              .count();
-      time = after - before;
-      std::cout << "create_user " << time << std::endl;
+        std::string str(length,0);
+        auto it = str.begin();
+        for(auto i = 0; i < length; ++i ) {
+            *it++ = charset[ duration_cast<microseconds>(system_clock::now().time_since_epoch()).count() % max_index ];
+        }
+        return str;
     }
 
-    // std::cout << "test_follow sequence1" << std::endl;
-    for (auto i = 0; i < num_active_users; i++) {
-      auto follower = std::to_string(i);
-      for (auto j = 0; j < num_follow; j++) {
-        auto followee = std::to_string((i + j + 1) % num_users);
-        before =
-            duration_cast<microseconds>(system_clock::now().time_since_epoch())
-                .count();
-        m_load_balancer.follow(follower, followee);
-        after =
-            duration_cast<microseconds>(system_clock::now().time_since_epoch())
-                .count();
-        time = after - before;
-        std::cout << "follow " << time << std::endl;
-      }
+    tos::Task<bool> sequence1(int num_users, int num_active_users, int num_follow, int num_post) {
+        std::shared_ptr<apache::thrift::transport::TTransport> socket(
+            new apache::thrift::transport::TSocket("10.0.1.10", LB_PORT));
+        std::shared_ptr<apache::thrift::transport::TTransport> transport(
+            new apache::thrift::transport::TBufferedTransport(socket));
+        std::shared_ptr<apache::thrift::protocol::TProtocol> protocol(
+            new apache::thrift::protocol::TBinaryProtocol(transport));
+        workerClient m_load_balancer_(protocol);
+        workerClient *m_load_balancer = &m_load_balancer_;
+
+        transport->open();
+
+        uint64_t before = 0;
+        uint64_t after = 0;
+        uint64_t t = 0;
+
+        // Generate random usernames
+        auto usernames = std::vector<std::string>();
+        for(auto n = 0; n < num_users; n++) {
+            auto s = random_string(10);
+            usernames.push_back(s);
+        }
+        
+        // Generate random posts
+        auto posts = std::vector<std::string>();
+        for(auto p = 0; p < num_post; p++) {
+            auto s = random_string(280);
+            posts.push_back(s);
+        }
+
+        // Test create user
+        for (auto u : usernames) {
+            before = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+            m_load_balancer->create_user(u);
+            after = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+            t = (after - before);
+            std::cout << "create_user " << t << std::endl;
+        }
+        
+        // Test follow
+        for (auto i = 0; i < num_active_users; i++) {
+            for (auto j = 0; j < num_follow; j++) {
+                before = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+                m_load_balancer->follow(usernames[i], usernames[(i + j + 1) % num_users]);
+                after = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+                t = (after - before);
+                std::cout << "follow " << t << std::endl;
+            }
+        }
+
+        // Test post tweet
+        for (auto i = 0; i < num_active_users; i++) {
+            for (auto post : posts) {
+                before = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+                m_load_balancer->post_tweet(usernames[i], post);
+                after = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+                t = (after - before);
+                std::cout << "post_tweet " << t << std::endl;
+            }
+        }
+
+        // Test unfollow
+        for (auto i = 0; i < num_active_users; i++) {
+            for (auto j = 0; j < num_follow; j++) {
+                before = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+                m_load_balancer->unfollow(usernames[i], usernames[(i + j + 1) % num_users]);
+                after = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+                t = (after - before);
+                std::cout << "unfollow " << t << std::endl;
+            }
+        }
+
+        // Test delete_user
+        for (auto u : usernames) {
+            before = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+            m_load_balancer->delete_user(u);
+            after = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+            t = (after - before);
+            std::cout << "delete_user " << t << std::endl;
+        }
+
+        co_return true;
     }
 
-    // std::cout << "post_tweet sequence1" << std::endl;
-    for (auto i = 0; i < num_active_users; i++) {
-      auto username = std::to_string(i);
-      for (auto j = 0; j < num_post; j++) {
-        auto post = "post" + std::to_string(j);
-        before =
-            duration_cast<microseconds>(system_clock::now().time_since_epoch())
-                .count();
-        m_load_balancer.post_tweet(username, post);
-        after =
-            duration_cast<microseconds>(system_clock::now().time_since_epoch())
-                .count();
-        time = after - before;
-        std::cout << "post_tweet " << time << std::endl;
-      }
-    }
+  // bool sequence1(int num_users, int num_active_users, int num_follow,
+  //                int num_post) {
+  //   std::shared_ptr<apache::thrift::transport::TTransport> socket(
+  //       new apache::thrift::transport::TSocket("10.0.1.10", LB_PORT));
+  //   std::shared_ptr<apache::thrift::transport::TTransport> transport(
+  //       new apache::thrift::transport::TBufferedTransport(socket));
+  //   std::shared_ptr<apache::thrift::protocol::TProtocol> protocol(
+  //       new apache::thrift::protocol::TBinaryProtocol(transport));
+  //   workerClient m_load_balancer(protocol);
 
-    // std::cout << "test_unfollow sequence1" << std::endl;
-    for (auto i = 0; i < num_active_users; i++) {
-      auto follower = std::to_string(i);
-      for (auto j = 0; j < num_follow; j++) {
-        auto followee = std::to_string((i + j + 1) % num_users);
-        before =
-            duration_cast<microseconds>(system_clock::now().time_since_epoch())
-                .count();
-        m_load_balancer.unfollow(follower, followee);
-        after =
-            duration_cast<microseconds>(system_clock::now().time_since_epoch())
-                .count();
-        time = after - before;
-        std::cout << "test_unfollow " << time << std::endl;
-      }
-    }
+  //   transport->open();
 
-    // std::cout << "test_delete_user sequence1" << std::endl;
-    for (auto i = 0; i < num_users; i++) {
-      auto username = std::to_string(i);
-      before =
-          duration_cast<microseconds>(system_clock::now().time_since_epoch())
-              .count();
-      m_load_balancer.delete_user(username);
-      after =
-          duration_cast<microseconds>(system_clock::now().time_since_epoch())
-              .count();
-      time = after - before;
-      std::cout << "test_delete_user " << time << std::endl;
-    }
-    transport->close();
-    return true;
-  }
+  //   uint64_t before = 0;
+  //   uint64_t after = 0;
+  //   uint64_t time = 0;
+
+  //   // std::cout << "test_create_user sequence1" << std::endl;
+  //   for (auto i = 0; i < num_users; i++) {
+  //     auto username = std::to_string(i);
+  //     before =
+  //         duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+  //     m_load_balancer.create_user(username);
+  //     after =
+  //         duration_cast<microseconds>(system_clock::now().time_since_epoch())
+  //             .count();
+  //     time = after - before;
+  //     std::cout << "create_user " << time << std::endl;
+  //   }
+
+  //   // std::cout << "test_follow sequence1" << std::endl;
+  //   for (auto i = 0; i < num_active_users; i++) {
+  //     auto follower = std::to_string(i);
+  //     for (auto j = 0; j < num_follow; j++) {
+  //       auto followee = std::to_string((i + j + 1) % num_users);
+  //       before =
+  //           duration_cast<microseconds>(system_clock::now().time_since_epoch())
+  //               .count();
+  //       m_load_balancer.follow(follower, followee);
+  //       after =
+  //           duration_cast<microseconds>(system_clock::now().time_since_epoch())
+  //               .count();
+  //       time = after - before;
+  //       std::cout << "follow " << time << std::endl;
+  //     }
+  //   }
+
+  //   // std::cout << "post_tweet sequence1" << std::endl;
+  //   for (auto i = 0; i < num_active_users; i++) {
+  //     auto username = std::to_string(i);
+  //     for (auto j = 0; j < num_post; j++) {
+  //       auto post = "post" + std::to_string(j);
+  //       before =
+  //           duration_cast<microseconds>(system_clock::now().time_since_epoch())
+  //               .count();
+  //       m_load_balancer.post_tweet(username, post);
+  //       after =
+  //           duration_cast<microseconds>(system_clock::now().time_since_epoch())
+  //               .count();
+  //       time = after - before;
+  //       std::cout << "post_tweet " << time << std::endl;
+  //     }
+  //   }
+
+  //   // std::cout << "test_unfollow sequence1" << std::endl;
+  //   for (auto i = 0; i < num_active_users; i++) {
+  //     auto follower = std::to_string(i);
+  //     for (auto j = 0; j < num_follow; j++) {
+  //       auto followee = std::to_string((i + j + 1) % num_users);
+  //       before =
+  //           duration_cast<microseconds>(system_clock::now().time_since_epoch())
+  //               .count();
+  //       m_load_balancer.unfollow(follower, followee);
+  //       after =
+  //           duration_cast<microseconds>(system_clock::now().time_since_epoch())
+  //               .count();
+  //       time = after - before;
+  //       std::cout << "test_unfollow " << time << std::endl;
+  //     }
+  //   }
+
+  //   // std::cout << "test_delete_user sequence1" << std::endl;
+  //   for (auto i = 0; i < num_users; i++) {
+  //     auto username = std::to_string(i);
+  //     before =
+  //         duration_cast<microseconds>(system_clock::now().time_since_epoch())
+  //             .count();
+  //     m_load_balancer.delete_user(username);
+  //     after =
+  //         duration_cast<microseconds>(system_clock::now().time_since_epoch())
+  //             .count();
+  //     time = after - before;
+  //     std::cout << "test_delete_user " << time << std::endl;
+  //   }
+  //   transport->close();
+  //   return true;
+  // }
 
   bool start() override {
     sequence1(10000, 500, 20, 20);

23c76420cb92e2c3adb3593a1a3e57a5db05be95
Sierra Wang <sierrawang@ucsb.edu>
2022-06-24 17:46:30 -0700

    scripts

---
+++ benchmark/fedora_scripts/run_load_balancer_host.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd ../../load_balancer/
+
+make clean
+
+make
+
+./write_endpoint > ../benchmark/fedora_results/fedora_BEPS_client_host_output.txt &
+
+python3 test_write_endpoint3.py
+
+cd ../benchmark/fedora_scripts/
+
+python3 parse_BEPS_output.py ../fedora_results/fedora_BEPS_client_host_output.txt > ../fedora_results/fedora_BEPS_client_host_parsed.txt

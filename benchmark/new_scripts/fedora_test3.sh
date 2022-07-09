37c27d6d123e56629a1fc611ad643e6c9b739aec
Sierra Wang <sierrawang@ucsb.edu>
2022-07-09 18:53:08 +0000

    fedora_test3

---
+++ benchmark/new_scripts/fedora_test3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker rm -f $(docker ps -a)
+
+# Compile everything in a container then kill it
+docker run --net mynet --name make_load_balancer_con -v $HOME/AmbienceBenchmarksThrift:/root/app -it thrift_img /bin/bash benchmark/new_scripts/make_lb_logging.sh
+docker rm -f make_load_balancer_con
+
+docker run -p 9089:9089 --net mynet --name server_con -v $HOME/AmbienceBenchmarksThrift:/root/app thrift_img /bin/bash benchmark/new_scripts/run_server_side_services.sh $1 &
+

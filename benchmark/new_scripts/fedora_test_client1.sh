79454a4114b40c98851a727ff887b2c907a6b302
Sierra Wang <sierrawang@ucsb.edu>
2022-07-09 12:01:49 -0700

    fix client

--- benchmark/new_scripts/fedora_test_client1.sh
+++ benchmark/new_scripts/fedora_test_client1.sh
@@ -6,4 +6,12 @@ make clean
 
 make -f Makefile.log
 
-./write_endpoint 10.0.1.10 > ../benchmark/new_results/$1_we_output.txt
+./write_endpoint 10.0.1.10 > ../benchmark/new_results/$1_we_output.txt &
+
+sleep 2
+
+python3 test_write_endpoint.py localhost
+
+cd ../benchmark/new_scripts/
+
+python3 parse_fedora_test1.py ../new_results/fedora_test5_we_output.txt > ../new_results/fedora_test5_we_parsed.txt

782fc55a968133a2cead817504366f6e6f652008
Sierra Wang <sierrawang@ucsb.edu>
2022-07-12 17:35:09 +0000

    one group 1 test results

---
+++ benchmark/new_scripts/run_all_services_no_log.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd new_load_balancer
+
+make clean
+
+make
+
+./read_endpoint &
+sleep 1
+
+./tweet_db &
+sleep 1
+
+./user_db one_con &
+sleep 1
+
+for (( i = 0; i < 5; i++ ))
+do 
+	./worker $i one_con one_con one_con &
+done
+
+sleep 1
+
+./load_balancer one_con > ../benchmark/new_results/$1_lb_output.txt &
+
+sleep 1
+
+./write_endpoint one_con > ../benchmark/new_results/$1_we_output.txt

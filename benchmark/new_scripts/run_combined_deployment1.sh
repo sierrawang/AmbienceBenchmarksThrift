e2c71dd24ed8ed5120e459b150ef7f86dba62aac
Sierra Wang <sierrawang@ucsb.edu>
2022-07-12 21:07:53 +0000

    results

---
+++ benchmark/new_scripts/run_combined_deployment1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker rm -f $(docker ps -aq)
+
+docker run -p 9121:9121 --net mynet --name one_con -v $HOME/AmbienceBenchmarksThrift:/root/app thrift_img /bin/bash benchmark/new_scripts/run_all_services.sh $1

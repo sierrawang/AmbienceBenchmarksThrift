aaa9aebac25e38b1f9cde7105c318f17ff544fe5
Sierra Wang <sierrawang@ucsb.edu>
2022-07-19 22:24:55 -0700

    test agent used for final results

--- poll/test_poll_agent.py
+++ poll/test_poll_agent.py
@@ -9,7 +9,7 @@ from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
-ip = "build.a6e.org"
+ip = "10.0.1.10"
 port = 9093
 
 # Make socket
@@ -23,9 +23,42 @@ agnt = agent.Client(protocol)
 # Connect!
 transport.open()
 
-num_iterations = 100000
+num_iterations = 10000
 agnt.start(num_iterations)
-print("Finished poll agent")
 
 transport.close()
 
+port = 9094
+
+# Make socket
+transport = TSocket.TSocket(ip, port)
+transport = TTransport.TBufferedTransport(transport)
+protocol = TBinaryProtocol.TBinaryProtocol(transport)
+
+# Create a client to use the protocol encode
+agnt = agent.Client(protocol)
+
+# Connect!
+transport.open()
+
+buffer_size = 512
+agnt.start(buffer_size)
+print("Finished 512 test")
+
+buffer_size = 1024
+agnt.start(buffer_size)
+print("Finished 1024 test")
+
+buffer_size = 2048
+agnt.start(buffer_size)
+print("Finished 2048 test")
+
+buffer_size = 4096
+agnt.start(buffer_size)
+print("Finished 4096 test")
+
+buffer_size = 8192
+agnt.start(buffer_size)
+print("Finished 8192 test")
+
+transport.close()
